1b9e12653294921b851d166a314fb5b6a0eeb128
vtu30028-lab <vtu30028@veltech.edu.in>
2026-06-29 21:24:15 +0530

    updat api url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://smartpg-gyvw.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
