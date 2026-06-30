b12e13aad90fa9bc28ac1b7bb01e2dbe9848697a
vtu30028-lab <vtu30028@veltech.edu.in>
2026-06-30 12:42:41 +0530

    Update api.ts to include withCredentials for CORS

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://smartpg-gyvw.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
+  withCredentials: true
 });
 
 api.interceptors.request.use((config) => {
